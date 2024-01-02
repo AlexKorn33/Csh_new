0e9a9f65eadd84df6d2fa002a42eb3346f9e467a
AlexKorn <moray@mail2000.ru>
2024-01-02 16:12:13 +0700

    Vebinar_work2_restart

---
+++ Vebinar1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Console.Clear();
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n + 10);
+*/
+
+// Console.Clear();
+// Console.Write("Введите число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (b * b ==a)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+
+// Console.Clear();
+// Console.Write("Введите число:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int i = n * (-1);
+// while (i <= n)
+// {
+//     Console.Write(i + ' ');
+//     i = i + 1;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите число:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int i = n * (-1);
+// while (i <= n)
+// {
+//     Console.Write(i + " ");
+//     i+= 1; //i = i + 1;
+// }
+
+Console.Clear();
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 100; //456 / 100 -> 4(56 ost) = 4
+int n3 = n % 10; //456 % -> 45(6 ost) = 6
+Console.WriteLine(n1 + n3);
