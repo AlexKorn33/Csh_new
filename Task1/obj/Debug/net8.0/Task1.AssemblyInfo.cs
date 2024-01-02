7b990af772e894b6df3cf5a2e5ae94865f519aa9
AlexKorn <moray@mail2000.ru>
2024-01-02 10:38:02 +0700

    Add new data

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1f4b6d1ed52feb9e56444ea95af6a45a8059d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
