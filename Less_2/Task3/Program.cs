f1189317a73938b0692cbf6331b67c3123cf9d6f
AlexKorn <moray@mail2000.ru>
2024-01-13 11:33:39 +0700

    add Less_2:Task3

---
+++ Less_2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] arr = { 2, 5, 13, 16, 35, 7, 8, 197, 24, 25 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if(arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
